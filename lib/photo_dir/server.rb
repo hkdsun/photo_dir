7811a79a5b76786cb26434c8bdbcd0609772483a
Hormoz K <hkheradm@gmail.com>
2021-02-26 14:29:46 -0500

    better styling

--- lib/photo_dir/server.rb
+++ lib/photo_dir/server.rb
@@ -2,14 +2,15 @@ require 'sinatra'
 
 module PhotoDir
   class Server < Sinatra::Base
-    set :public_folder, ENV['PHOTO_DIR']
-    set :env, 'production'
+    set :photo_dir, Proc.new { ENV['PHOTO_DIR'] || "./example_dir" }
+    set :public_folder, Proc.new { settings.photo_dir }
     set :bind, "0.0.0.0"
 
     get '/' do
-      img_files = Dir.entries(ENV['PHOTO_DIR'])
+      photo_dir = settings.photo_dir
+      img_files = Dir.entries(photo_dir)
       img_files = img_files.select { |f| f.end_with?(".jpg") || f.end_with?(".png") }
-      img_files = img_files.sort_by { |f| File.mtime("#{ENV['PHOTO_DIR']}/#{f}") }.reverse
+      img_files = img_files.sort_by { |f| File.mtime("#{photo_dir}/#{f}") }.reverse
       erb :page, locals: { imgs: img_files }
     end
   end
